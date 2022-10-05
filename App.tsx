3f9e8fa7fb0d481becae9605be258656ca930bb2
edinelson luis de sousa junior <edinelsonjr.ufopa@gmail.com>
2022-10-05 14:35:08 -0300

    criando estilos globais

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-
-import React from 'react';
-import Dashboard from './src/screens/Dashboard';
+import React from "react";
+import Dashboard from "./src/screens/Dashboard";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/global/styles/theme";
 
 export default function App() {
   return (
-    <Dashboard/>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
-
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,15 +1,16 @@
 import styled from "styled-components/native";
+import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
     align-items: center;
     justify-content: center;
     flex: 1;
-    background-color: #ff1255;
+    background-color: ${({theme})=>theme.colors.background};
 `;
 
 export const Title = styled.Text`
     font-size: 22px;
-    color: #000000;
+    color: ${({theme})=>theme.colors.title};
     font-weight: bolder;
 `; 
 
